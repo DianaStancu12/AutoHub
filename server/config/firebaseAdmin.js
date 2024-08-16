7f624855c46a4daae011395e009450bb0e0661af
unknown <stancudiana21@stud.ase.ro>
2024-08-16 19:28:45 +0300

    stergere continut

--- server/config/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); 
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://licenta-3eb72.firebaseio.com'
-});
-
-module.exports = admin;
